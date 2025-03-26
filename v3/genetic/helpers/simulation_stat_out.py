88dcfec0113ef0116a6ba58e43cf9a3c1d380578
dev-zgr <ozgurkamalipersonal@gmail.com>
2025-03-26 18:51:16 +0300

    Makespan profiling added for genetic v3

--- v3/genetic/helpers/simulation_stat_out.py
+++ v3/genetic/helpers/simulation_stat_out.py
@@ -3,9 +3,10 @@ import json
 from v3.genetic import Constants as cnst
 
 
-def simulation_stat_out(ToD, num_of_jobs, simulation_file, profiling_results):
+def simulation_stat_out(ToD, num_of_jobs, simulation_file, profiling_results,makespan_results):
     try:
         ToD_str = ', '.join(map(str, ToD))
+        makespan_str = ', '.join(map(str, makespan_results))
         profiling_time_stamp_string = ', '.join(map(lambda y: f"{y["exec_time"]:.3f}", profiling_results))
         cpu_profiling_time_stamp_string = ', '.join(map(lambda y: f"{y["cpu_exec_time"]:.3f}", profiling_results))
         memory_profiling_string = ', '.join(map(lambda y: str(y["memory_usage"]), profiling_results))
@@ -31,6 +32,7 @@ def simulation_stat_out(ToD, num_of_jobs, simulation_file, profiling_results):
             "simulation_id": simulation_id,
             "simulation_distribution": simulation_distribution,
             "ToD": ToD_str,
+            "makespan": makespan_str,
             "time_profile": profiling_time_stamp_string,
             "cpu_profile": cpu_profiling_time_stamp_string,
             "memory_profile": memory_profiling_string,
